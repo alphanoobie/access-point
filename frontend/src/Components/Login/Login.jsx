18b1cdcd2ff4a259262e6cc1c84712495c6c0bf7
alphanoobie <hussaingadi.hg@gmail.com>
2022-12-01 00:27:31 +0530

    login integration

--- frontend/src/Components/Login/Login.jsx
+++ frontend/src/Components/Login/Login.jsx
@@ -1,13 +1,31 @@
 import React from "react";
 import styles from "./Login.module.css";
+import {toast} from "react-toastify"
+import axios from "axios";
+import { useState } from "react";
 
-const login = () => {
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+        const { data } = await axios.post(`http://localhost:3001/api/login/`, { email, password });
+        console.log(data);
+        toast("Login Successful");
+        window.localStorage.setItem("user", JSON.stringify(data))
+    } catch (err) {
+      console.log(err);
+      toast(err.response.data)
+    }
+  };
   return (
     <div className={styles.logincontianer}>
       <div className={styles.header}>
         <h2>Login</h2>
       </div>
-      <form className={styles.loginform}>
+      <form className={styles.loginform} onSubmit={handleSubmit}>
         <label className={styles.labels}>Email</label>
         <input
           className={styles.inputs}
@@ -15,6 +33,8 @@ const login = () => {
           id="email"
           name="email"
           placeholder="youremail@mail.com"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
 
         <label className={styles.labels}>Password</label>
@@ -24,6 +44,8 @@ const login = () => {
           id="password"
           name="password"
           placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
         />
 
         <button className={styles.submitbutton}>Submit</button>
@@ -32,4 +54,4 @@ const login = () => {
   );
 };
 
-export default login;
+export default Login;
