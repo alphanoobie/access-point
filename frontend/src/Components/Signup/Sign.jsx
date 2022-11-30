0766707331fe0a9af3310a3f336a0fa4622525f4
ajit6609 <58482666+ajit6609@users.noreply.github.com>
2022-11-30 15:11:33 +0530

    Sign up

--- frontend/src/Components/Signup/Sign.jsx
+++ frontend/src/Components/Signup/Sign.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from "react";
+import styles from "./Sign.module.css";
 
 const Sign = () => {
   return (
-    <div>Sign</div>
-  )
-}
+    <div className={styles.signcontianer}>
+      <div className={styles.header}>
+        <h2>Sign Up</h2>
+      </div>
+      <form className={styles.signform}>
+        <label className={styles.labels}>First Name</label>
+        <input
+          className={styles.inputs}
+          type="text"
+          id="f_name"
+          name="f_name"
+          placeholder="First Name"
+        />
+        <label className={styles.labels}>Last Name</label>
+        <input
+          className={styles.inputs}
+          type="text"
+          id="l_name"
+          name="l_name"
+          placeholder="Last Name"
+        />
+        <label className={styles.labels}>Email</label>
+        <input
+          className={styles.inputs}
+          type="email"
+          id="email"
+          name="email"
+          placeholder="youremail@mail.com"
+        />
 
-export default Sign
+        <label className={styles.labels}>Create Password</label>
+        <input
+          className={styles.inputs}
+          type="password"
+          id="password"
+          name="password"
+          placeholder="password"
+        />
+
+        <button className={styles.submitbutton}>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default Sign;
