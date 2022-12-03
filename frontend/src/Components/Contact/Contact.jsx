7ef7736ed7ce5f4f3d38b8040eaa17c9c73f3cfe
roxter207 <66411952+roxter207@users.noreply.github.com>
2022-12-03 19:41:41 +0530

    tried contact form bacnkend please check

--- frontend/src/Components/Contact/Contact.jsx
+++ frontend/src/Components/Contact/Contact.jsx
@@ -1,18 +1,38 @@
 import React from "react";
 import {
-  Typography,
-  Card,
-  CardContent,
-  Grid,
-  TextField,
-  Button,
-} from "@mui/material";
+  Typography, Card,CardContent, Grid,  TextField,  Button,} from "@mui/material";
+import {toast} from "react-toastify"
+import axios from "axios";
+import { useState } from "react";
+
+
 
 const Contact = () => {
+  const [fname, setFname] = useState("")
+  const [lname, setLname] = useState("")
+  const [email, setEmail] = useState("")
+  const [phone, setPhone] = useState("")
+  const [message, setMessage] = useState("")
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try{
+      const {data} = await axios.post('http://localhost:3001/api/contact/', {fname, lname, email, phone, message});
+      toast("Thank you! Our team will get back to you soon.")
+      console.log(data);
+      window.localStorage.setItem("user",JSON.stringify(data))
+    }
+    catch(err){
+      console.log(err);
+      toast(err.response.data)
+    }
+  }
+
+
   return (
     <div>
       <Typography gutterBottom variant="h3" align="center">
-        Contact Us
+        Contact Us 
       </Typography>
       <Grid>
         <Card style={{ maxWidth: 450, padding: "5px 5px", margin: "0 auto" }}>
@@ -25,7 +45,7 @@ const Contact = () => {
             Fill up the form and our team will get back to you within 24 hours.
           </Typography>
           <CardContent>
-            <form style={{marginTop: "20px"}}>
+            <form style={{marginTop: "20px"}} onSubmit={handleSubmit}>
               <Grid container spacing={1}>
                 <Grid xs={12} sm={6} item>
                   <TextField
@@ -34,6 +54,7 @@ const Contact = () => {
                     variant="outlined"
                     fullWidth
                     required
+                    onChange={(e)=>setFname(e.target.value)}
                   />
                 </Grid>
                 <Grid xs={12} sm={6} item>
@@ -43,6 +64,7 @@ const Contact = () => {
                     variant="outlined"
                     fullWidth
                     required
+                    onChange={(e)=>setLname(e.target.value)}
                   />
                 </Grid>
                 <Grid item xs={12}>
@@ -53,6 +75,7 @@ const Contact = () => {
                     variant="outlined"
                     fullWidth
                     required
+                    onChange={(e)=>setEmail(e.target.value)}
                   />
                 </Grid>
                 <Grid item xs={12}>
@@ -63,6 +86,7 @@ const Contact = () => {
                     variant="outlined"
                     fullWidth
                     required
+                    onChange={(e)=>setPhone(e.target.value)}
                   />
                 </Grid>
                 <Grid item xs={12}>
@@ -74,6 +98,7 @@ const Contact = () => {
                     variant="outlined"
                     fullWidth
                     required
+                    onChange={(e)=>setMessage(e.target.value)}
                   />
                 </Grid>
                 <Grid item xs={12}>
