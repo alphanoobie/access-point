c8869c3f2336e48e187333457d9ac09ddc03b6ea
ajit6609 <58482666+ajit6609@users.noreply.github.com>
2022-12-12 14:52:59 +0530

    service form new ui

--- frontend/src/Components/Service/SevicePopForm.jsx
+++ frontend/src/Components/Service/SevicePopForm.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import {
-  Typography,
   Card,
   CardContent,
   Grid,
-  TextField,
   Button,
+  Radio,
+  Typography,
+  CardActionArea,
+  CardActions,
 } from "@mui/material";
 import { toast } from "react-toastify";
 import axios from "axios";
@@ -15,21 +17,22 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const ServicePopForm = (props) => {
-  const [fName, setfName] = useState("");
-  const [lName, setlName] = useState("");
-  const [phone, setphone] = useState("");
-  const [email, setemail] = useState("");
-  const [ServiceMessage, setServiceMessage] = useState("");
+  const [service, setService] = useState("");
   const navigate = useNavigate();
+
+  const handleChange = (event) => {
+    setService(event.target.value);
+  };
+
+  const handleCardClick = (service_name) => {
+    setService(service_name);
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       //   const { data } = await axios.post(`http://localhost:3001/api/login/`, {
-      //     fName,
-      //     lName,
-      //     email,
-      //     phone,
-      //     ServiceMessage,
+      //     service
       //   });
       //   console.log(data);
       toast("Service Requested Successfully");
@@ -53,13 +56,17 @@ const ServicePopForm = (props) => {
             color="textSecondary"
             variant="h3"
             align="center"
-            style={{ padding: "1%" }}
+            style={{ padding: "1%", width: "700px" }}
           >
             We are here for you!
           </Typography>
           <Grid>
             <Card
-              style={{ maxWidth: 450, padding: "10px 10px", margin: "0 auto" }}
+              style={{
+                maxWidth: "700px",
+                padding: "10px 10px",
+                margin: "0 auto",
+              }}
             >
               <div
                 style={{
@@ -87,83 +94,119 @@ const ServicePopForm = (props) => {
               </Typography>
               <CardContent>
                 <form onSubmit={handleSubmit} style={{ marginTop: "20px" }}>
-                  <Grid container spacing={1}>
-                    <Grid xs={12} sm={6} item>
-                      <TextField
-                        placeholder="Enter first name"
-                        label="First Name"
-                        variant="outlined"
-                        value={fName}
-                        onChange={(event) => setfName(event.target.value)}
-                        fullWidth
-                        required
-                        // onChange={(e)=>setFname(e.target.value)}
-                      />
-                    </Grid>
-                    <Grid xs={12} sm={6} item>
-                      <TextField
-                        placeholder="Enter last name"
-                        label="Last Name"
-                        variant="outlined"
-                        value={lName}
-                        onChange={(event) => setlName(event.target.value)}
-                        fullWidth
-                        required
-                        // onChange={(e)=>setLname(e.target.value)}
-                      />
-                    </Grid>
-                    <Grid item xs={12}>
-                      <TextField
-                        type="email"
-                        placeholder="Enter email"
-                        label="Email"
-                        variant="outlined"
-                        value={email}
-                        onChange={(event) => setemail(event.target.value)}
-                        fullWidth
-                        required
-                        // onChange={(e)=>setEmail(e.target.value)}
-                      />
-                    </Grid>
-                    <Grid item xs={12}>
-                      <TextField
-                        type="number"
-                        placeholder="Enter phone number"
-                        label="Phone"
-                        variant="outlined"
-                        value={phone}
-                        onChange={(event) => setphone(event.target.value)}
-                        fullWidth
-                        required
-                        // onChange={(e)=>setPhone(e.target.value)}
-                      />
-                    </Grid>
-                    <Grid item xs={12}>
-                      <TextField
-                        label="Message"
-                        multiline
-                        rows={4}
-                        placeholder="Type your message here"
-                        variant="outlined"
-                        value={ServiceMessage}
-                        onChange={(event) =>
-                          setServiceMessage(event.target.value)
-                        }
-                        fullWidth
-                        required
-                        // onChange={(e)=>setMessage(e.target.value)}
-                      />
-                    </Grid>
-                    <Grid item xs={12}>
-                      <Button
-                        type="submit"
-                        variant="contained"
-                        color="primary"
-                        fullWidth
-                      >
-                        Submit
-                      </Button>
-                    </Grid>
+                  <Card
+                    onClick={() => handleCardClick("repair")}
+                    sx={{ maxWidth: 800, margin: "2%", padding: "1%" }}
+                  >
+                    <CardActionArea>
+                      <CardContent>
+                        <Radio
+                          checked={service === "repair"}
+                          onChange={handleChange}
+                          value="repair"
+                          name="radio-buttons"
+                          style={{ verticalAlign: "sub" }}
+                        />
+                        <Typography
+                          display="inline"
+                          gutterBottom
+                          variant="h5"
+                          component="div"
+                        >
+                          Repair & Maintainance
+                        </Typography>
+                        <Typography
+                          ml={5}
+                          variant="body2"
+                          color="text.secondary"
+                        >
+                          Starting from 399/- onwards. Additional cost depends
+                          on parts and equipment.
+                        </Typography>
+                      </CardContent>
+                    </CardActionArea>
+                    <CardActions></CardActions>
+                  </Card>
+
+                  <Card
+                    onClick={() => handleCardClick("turnkey")}
+                    sx={{ maxWidth: 800, margin: "2%", padding: "1%" }}
+                  >
+                    <CardActionArea>
+                      <CardContent>
+                        <Radio
+                          checked={service === "turnkey"}
+                          onChange={handleChange}
+                          value="turnkey"
+                          name="radio-buttons"
+                          size="small"
+                          style={{ verticalAlign: "sub" }}
+                        />
+                        <Typography
+                          display="inline"
+                          gutterBottom
+                          variant="h5"
+                          component="div"
+                          mt="10"
+                        >
+                          TURNKEY PROJECTS
+                        </Typography>
+                        <Typography
+                          ml={5}
+                          variant="body2"
+                          color="text.secondary"
+                        >
+                          Starting from 499/- onwards. Additional cost depends
+                          on parts and equipment.
+                        </Typography>
+                      </CardContent>
+                    </CardActionArea>
+                    <CardActions></CardActions>
+                  </Card>
+
+                  <Card
+                    onClick={() => handleCardClick("staffing")}
+                    sx={{ maxWidth: 800, margin: "2%", padding: "1%" }}
+                  >
+                    <CardActionArea>
+                      <CardContent>
+                        <Radio
+                          checked={service === "staffing"}
+                          onChange={handleChange}
+                          value="staffing"
+                          name="radio-buttons"
+                          style={{ verticalAlign: "sub" }}
+                        />
+                        <Typography
+                          display="inline"
+                          gutterBottom
+                          variant="h5"
+                          component="div"
+                        >
+                          STAFFING SOLUTIONS
+                        </Typography>
+                        <Typography
+                          ml={5}
+                          variant="body2"
+                          color="text.secondary"
+                        >
+                          Starting from 599/- onwards. Additional cost depends
+                          on parts and equipment.
+                        </Typography>
+                      </CardContent>
+                    </CardActionArea>
+                    <CardActions></CardActions>
+                  </Card>
+
+                  <Grid item xs={12}>
+                    <Button
+                      type="submit"
+                      variant="contained"
+                      color="primary"
+                      fullWidth
+                    >
+                      Submit
+                    </Button>
                   </Grid>
                 </form>
               </CardContent>
