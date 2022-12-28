f1facf4ebc6f27f859b47c9516c703dccd190da5
alphanoobie <hussaingadi.hg@gmail.com>
2022-12-28 13:19:57 +0530

    send customer email backend

--- backend/controllers/contactUs.js
+++ backend/controllers/contactUs.js
@@ -1,6 +1,15 @@
 const ContactUs = require("../models/contactUs");
+require("dotenv").config();
+const nodemailer = require("nodemailer");
 
 const contactUs = async (req, res) => {
+  const transporter = nodemailer.createTransport({
+    service: "hotmail",
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASSWORD,
+    },
+  });
   try {
     const { fname, lname, email, phone, message } = req.body;
     const contact = new ContactUs({
@@ -11,6 +20,20 @@ const contactUs = async (req, res) => {
       message,
     });
     await contact.save();
+    const options = {
+      from: process.env.EMAIL,
+      to: email,
+      subject: "ACCESS POINT Contact Us Response",
+      text: "You have successfully filled the contact us form. Our Admin will get back to you shortly ",
+    };
+
+    transporter.sendMail(options, function (err, info) {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      console.log(info.response);
+    });
     console.log("contact form submitted");
     return res.json({ ok: true });
   } catch (err) {
@@ -21,11 +44,11 @@ const contactUs = async (req, res) => {
 
 const allContacts = async (req, res) => {
   try {
-    const allContacts = await ContactUs.find({})
-    res.json(allContacts)
+    const allContacts = await ContactUs.find({});
+    res.json(allContacts);
   } catch (error) {
     console.log(error);
-    res.status(400).send('Error, Try Again')
+    res.status(400).send("Error, Try Again");
   }
 };
 
